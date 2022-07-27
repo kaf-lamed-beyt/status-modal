f36a48f81da763ca5ce43e33c32337dc1f7fdef5
kaf-lamed-beyt <belac335@gmail.com>
2022-07-27 17:58:29 +0100

    chore: add appropriate loader to process sass files

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
         },
       },
       {
-        test: /\.s[ac]ss$/,
+        test: /\.scss$/,
         use: ["style-loader", "css-loader", "sass-loader", "postcss-loader"],
       },
     ],
